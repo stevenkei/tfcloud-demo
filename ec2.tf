34cf56b52e80f81110e9f3ab1140282283422483
Steven <stevenkei98@gmail.com>
2023-03-08 12:00:11 +0000

    Update ec2.tf
    refactor

--- ec2.tf
+++ ec2.tf
@@ -2,8 +2,10 @@ resource "aws_instance" "hello-world" {
 
  ami = "ami-0a313d6098716f372" 
  instance_type = "t2.micro"
- key_name = "terraformKP"
+ key_name = "Terraformcld"
  tags = {
    Name = "Hello world"
    Project = "Terraform-cloud-demo"
- }}
+}
+
+}
