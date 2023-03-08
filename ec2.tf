dc5ba1c0ccb861a1ff3f17eb9e8d4bc07e223be4
Steven <stevenkei98@gmail.com>
2023-03-08 10:46:52 +0000

    Create ec2.tf
    creating a ec2 instance in aws via terraform

---
+++ ec2.tf
@@ -0,0 +1,10 @@
+resource "aws_instance" "hello-world" {
+
+ ami = "ami-0a313d6098716f372" 
+ instance_type = "t2.micro"
+ vpc_security_group_ids = ["${aws_security_group.webserver_sg.id}"]
+ key_name = "terraformKP"
+ tags = {
+	 Name = "Hello world"
+   Project = "Terraform-cloud-demo"
+ }
