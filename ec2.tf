ae0bbca84779fbc643933d26af47edabe55070c8
Steven <stevenkei98@gmail.com>
2023-03-08 10:47:19 +0000

    Update ec2.tf
    rm security group var

--- ec2.tf
+++ ec2.tf
@@ -2,7 +2,6 @@ resource "aws_instance" "hello-world" {
 
  ami = "ami-0a313d6098716f372" 
  instance_type = "t2.micro"
- vpc_security_group_ids = ["${aws_security_group.webserver_sg.id}"]
  key_name = "terraformKP"
  tags = {
 	 Name = "Hello world"
